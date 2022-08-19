95742b661f09e390ac3e360080eec10a650b8d93
Ryan Martin <ryan.martin@resmodsol.com>
2022-08-19 01:21:59 -0400

    get the draft state

--- index.js
+++ index.js
@@ -49,6 +49,7 @@ const main = async () => {
   core.debug(`pr: ${JSON.stringify(pr, null, 2)}`)
   core.setOutput('number', pr ? pr.number : '')
   core.setOutput('head-sha', pr ? pr.head.sha : '')
+  core.setOutput('draft', pr ? pr.draft : '')
 }
 
 main().catch(err => core.setFailed(err.message))
